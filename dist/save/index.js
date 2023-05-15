22a669b6671f2c0a580d3f9d8ad85fee7609a722
DongHo Jung <dongho971220@gmail.com>
2023-05-16 01:39:00 +0900

    build

--- dist/save/index.js
+++ dist/save/index.js
@@ -3134,8 +3134,8 @@ function saveImpl() {
             yield utils.exec(`touch ${cachePath}/${key}/.partialCache`);
             for (const path of paths) {
                 const pathKey = crypto.createHash("md5").update(path).digest("hex");
-                yield utils.exec(`rsync -a --perms --chmod=777 "${path}"/ "${cachePath}/${pathKey}" `);
-                core.info(`Cache saved to key: "${path}" -> "${key}/${pathKey}"`);
+                yield utils.exec(`tar -czf "${cachePath}/${key}/${pathKey}.tar.gz" "${path}"`);
+                core.info(`Cache saved to key: "${path}" -> "${cachePath}/${key}/${pathKey}.tar.gz"`);
             }
             yield utils.exec(`rm ${cachePath}/${key}/.partialCache`);
         }
